a956e34cd74ec447983e13691f11e4c84bf6f1c4
Jason Rattigan <jason.rattigan@hotmail.com>
2025-12-07 10:38:18 +1100

    Add deployment script for AVD lab setup

---
+++ Microsoft/Azure-VirtualDesktop/scripts/gettingstarted/deploy-avd-lab.ps1
@@ -0,0 +1,3 @@
+./scripts/getting-started/00-deploy-foundation.ps1
+./scripts/getting-started/01-create-storage.ps1 -StorageAccountName "yourfslogixsa"
+./scripts/getting-started/02-deploy-hostpool.ps1
